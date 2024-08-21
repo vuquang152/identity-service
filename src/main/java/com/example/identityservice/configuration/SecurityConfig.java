df1b2e12ba033e513d7751c436535c94eb5cca7a
vuquang152 <quang.vh1502@gmail.com>
2024-08-21 16:11:21 +0700

    handle exception error code

--- src/main/java/com/example/identityservice/configuration/SecurityConfig.java
+++ src/main/java/com/example/identityservice/configuration/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
         httpSecurity.oauth2ResourceServer(oauth2 ->
                 oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
                         .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
         );
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
